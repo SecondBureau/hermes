8455a03a61b2efb770cfe64562ad60eba105b966
Gilles Crofils <gilles@secondbureau.com>
2012-07-19 08:36:31 +0800

    temp user seed

--- db/seeds/user.rb
+++ db/seeds/user.rb
@@ -4,3 +4,89 @@ User.create!(
   :email => 'jdoe@secondbureau.com',
   :created_at => Time.now,
   :updated_at => Time.now)
+
+  # encoding: UTF-8
+  User.create!(
+    :contact => 'John Doe',
+    :email => 'jdoe@secondbureau.com',
+    :created_at => Time.now,
+    :updated_at => Time.now)
+    
+    # encoding: UTF-8
+    User.create!(
+      :contact => 'John Doe',
+      :email => 'jdoe@secondbureau.com',
+      :created_at => Time.now,
+      :updated_at => Time.now)
+      
+      # encoding: UTF-8
+      User.create!(
+        :contact => 'John Doe',
+        :email => 'jdoe@secondbureau.com',
+        :created_at => Time.now,
+        :updated_at => Time.now)
+
+User.create!(
+:contact => 'Gregory VAN BELLINGHEN (EN)',
+:email => 'gvb@acapital.hk',
+:created_at => Time.now,
+:updated_at => Time.now)
+
+User.create!(
+:contact => 'Andre LOESEKRUG-PIETRI (EN)',
+:email => 'alp@acapital.hk',
+:created_at => Time.now,
+:updated_at => Time.now)
+
+
+User.create!(
+:contact => 'MAO Yanlai (EN)',
+:email => 'myl@acapital.fr',
+:created_at => Time.now,
+:updated_at => Time.now)
+
+User.create!(
+:contact => 'Gregory VAN BELLINGHEN (CN)',
+:email => 'gvb@acapital.hk',
+:created_at => Time.now,
+:updated_at => Time.now)
+
+
+User.create!(
+:contact => 'Andre LOESEKRUG-PIETRI (CN)',
+:email => 'alp@acapital.hk',
+:created_at => Time.now,
+:updated_at => Time.now)
+
+
+User.create!(
+:contact => 'MAO Yanlai (CN)',
+:email => 'myl@acapital.fr',
+:created_at => Time.now,
+:updated_at => Time.now)
+
+User.create!(
+:contact => 'Gregory VAN BELLINGHEN (EN)',
+:email => 'gvanbel@hotmail.com',
+:created_at => Time.now,
+:updated_at => Time.now)
+
+User.create!(
+:contact => 'Gregory VAN BELLINGHEN (CN)',
+:email => 'gvanbel@hotmail.com',
+:created_at => Time.now,
+:updated_at => Time.now)
+
+
+
+#UserMailer.newsletter(User.get(3), Template.get(1)).deliver
+#UserMailer.newsletter(User.get(4), Template.get(2)).deliver
+
+#UserMailer.newsletter(User.get(5), Template.get(1)).deliver
+#UserMailer.newsletter(User.get(6), Template.get(1)).deliver
+#UserMailer.newsletter(User.get(7), Template.get(1)).deliver
+#UserMailer.newsletter(User.get(8), Template.get(2)).deliver
+#UserMailer.newsletter(User.get(9), Template.get(2)).deliver
+#UserMailer.newsletter(User.get(10), Template.get(2)).deliver
+#UserMailer.newsletter(User.get(11), Template.get(1)).deliver
+#UserMailer.newsletter(User.get(12), Template.get(2)).deliver
